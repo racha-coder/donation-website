3e034e92c52a48ffc9de5b9a07e4876a1602a3f3
Charan Y <racha.coder@gmail.com>
2024-10-07 12:16:34 +0530

    add Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hero from './components/Hero';
 
 const App = () => {
   return (
     <div>
-      Write your code here
+      {/* Write your code here */}
+      <Hero />
     </div>
   )
 }
