9130886a8219a9a1ce9e1eb85c09bb350417c8b0
Charan Y <racha.coder@gmail.com>
2024-10-07 08:43:35 +0530

    comment the line of code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <div>
-      Write your code here
+      {/* Write your code here */}
     </div>
   )
 }
