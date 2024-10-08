5449c884ad43e3ce58023254058a93ac2db8ebde
Manasa-Mateti <manasamateti110@gmail.com>
2024-10-08 20:35:56 +0530

    create contact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
     <div>
       {/* Write your code here */}
       <Hero />
+      <Contact/>
     </div>
   )
 }
