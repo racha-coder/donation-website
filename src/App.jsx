164713d81e68923c064d0a09375a5e34f14ac7c5
Charan Y <racha.coder@gmail.com>
2024-10-09 10:27:57 +0530

    changes made after adding testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero';
+import Testimonials from './components/Testimonials';
 
 const App = () => {
   return (
     <div>
       {/* Write your code here */}
       <Hero />
+      <Testimonials />
     </div>
   )
 }
