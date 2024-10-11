a34fcb7392b28e1295c24744400c7e569eed6a6d
Charan Y <racha.coder@gmail.com>
2024-10-11 09:46:04 +0530

    change the bg colors of steps in donattion steps section

--- src/components/DonationProcess.jsx
+++ src/components/DonationProcess.jsx
@@ -8,42 +8,32 @@ const DonationProcess = () => {
 				Steps for Donation
 			</h1>
 			<div className="process-wrapper">
-				<div className="step" style={{'--bgc-color': 'tomato'}}>
-					<div className="step-icon">
-						{/* <i>i</i> */}
-					</div>
+				<div className="step" style={{'--percent': '20%'}}>
+					<div className="step-icon"></div>
 					<div className="description">
 						Click on donate button
 					</div>
 				</div>
-				<div className="step" style={{'--bgc-color': 'lightgoldenrodyellow'}}>
-					<div className="step-icon">
-						{/* <i>i</i> */}
-					</div>
+				<div className="step" style={{'--percent': '25%'}}>
+					<div className="step-icon"></div>
 					<div className="description">
 						Fill your details like name and email
 					</div>
 				</div>
-				<div className="step" style={{'--bgc-color': 'lightskyblue'}}>
-					<div className="step-icon">
-						{/* <i>i</i> */}
-					</div>
+				<div className="step" style={{'--percent': '30%'}}>
+					<div className="step-icon"></div>
 					<div className="description">
 						Enter Donation Amount
 					</div>
 				</div>
-				<div className="step" style={{'--bgc-color': 'lightgreen'}}>
-					<div className="step-icon">
-						{/* <i>i</i> */}
-					</div>
+				<div className="step" style={{'--percent': '35%'}}>
+					<div className="step-icon"></div>
 					<div className="description">
 						Select Payment method and make the Payment
 					</div>
 				</div>
-				<div className="step" style={{'--bgc-color': 'violet'}}>
-					<div className="step-icon">
-						{/* <i>i</i> */}
-					</div>
+				<div className="step" style={{'--percent': '40%'}}>
+					<div className="step-icon"></div>
 					<div className="description">
 						Collect the receipt
 					</div>
